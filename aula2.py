719f82e6f841693bd9d68ca6378299447f9ac9ac
feliperestivo <feliperestivo02@gmail.com>
2026-03-16 11:04:45 -0300

    teste

--- aula2.py
+++ aula2.py
@@ -26,3 +26,9 @@ num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 soma = num1 + num2
 print(f"A soma de {num1} e {num2} é igual a {soma}")
+
+# teste multiplicação
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+multiplicacao = num1 * num2
+print(f"A multiplicação de {num1} e {num2} é igual a {multiplicacao}")
